ea0c8ea1e79037920659b239b809bee98e983c46
Ankur Sharma <ankur.101045@gmail.com>
2024-05-26 01:04:01 +0530

    Added wrap for comments

--- src/Report.js
+++ src/Report.js
@@ -35,6 +35,7 @@ export default function Report() {
             name: 'Comments',
             selector: row => row.expenseName,
             sortable: true,
+            wrap: true
         },
         {
             name: 'Type',
